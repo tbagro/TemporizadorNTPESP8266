ab3442e8b122d4c3f1a1722449e88ec302d921a0
tbagro <tiagounemat@gmail.com>
2024-01-20 10:06:21 -0400

    Update TemporizadorReleAutomatico.ino

--- TemporizadorReleAutomatico.ino
+++ TemporizadorReleAutomatico.ino
@@ -3,7 +3,6 @@
 #include <NTPClient.h>
 #include <FS.h>
 
-
 const int relayPin = D5;  // Pino ao qual o relé está conectado
 bool relayState = false;  // Estado inicial do relé: desligado
 
@@ -11,28 +10,39 @@ WiFiManager wifiManager;
 WiFiUDP udp;
 NTPClient timeClient(udp, "a.st1.ntp.br", -4 * 3600, 60000);  // Fuso horário: GMT-4
 
-void hold(const unsigned int &ms) {
-  // Atraso não bloqueante
-  unsigned long m = millis();
-  while (millis() - m < ms) {
-    yield();
-  }
-}
-
 unsigned long pulseDuration = 40000;      // 40 segundos em milissegundos
-unsigned long intervalBetweenPulses = 1;  // 40 minutos
+unsigned long intervalBetweenPulses = 2;  // 40 minutos
 unsigned long lastActivationTime;
 
 const char *filePath = "/config.txt";
 
-void listFiles() {
-  Dir dir = SPIFFS.openDir("/");
-  while (dir.next()) {
-    Serial.print("Nome do arquivo: ");
-    Serial.println(dir.fileName());
-  }
-}
 
+void atualizarIntervaloEntrePulsos() {
+  Serial.println("Digite o novo valor para o intervalo entre pulsos (em minutos): ");
+
+  while (!Serial.available()) {
+    delay(100);  // Aguarda a entrada do usuário
+  }
+
+  // Lê o valor inserido pelo usuário
+  int novoIntervalo = Serial.parseInt();
+
+  // Atualiza o valor da variável global
+  intervalBetweenPulses = novoIntervalo;
+
+  // Salva o novo valor no arquivo
+  File configFile = SPIFFS.open(filePath, "w");
+  if (!configFile) {
+    Serial.println("Falha ao abrir o arquivo para escrita");
+    return;
+  }
+
+  configFile.println(intervalBetweenPulses);
+  configFile.close();
+
+  Serial.print("Intervalo entre pulsos atualizado para: ");
+  Serial.println(intervalBetweenPulses);
+}
 
 void saveLastPulseTime(unsigned long eventTime) {
   // Abre o arquivo para escrita
@@ -66,7 +76,7 @@ unsigned long readLastPulseTime() {
   }
 
   String content = configFile.readStringUntil('\n');
-  Serial.print("Conteúdo lido do arquivo: ");
+  Serial.print("valor salvo: ");
   Serial.println(content);
   configFile.close();
   return content.toInt();
@@ -89,40 +99,34 @@ void printLastPulseTime() {
 
   // Imprimir o valor do último acionamento
   Serial.print("Último acionamento registrado em: ");
+  Serial.println(lastPulseTime);
+  Serial.print("Último acionamento registrado em: ");
   Serial.println(buffer);
 }
 
-
-
-void acionarReleComTempo(unsigned int ms) {  // atraso em millis
-  // Ligar o relé (assumindo HIGH para ligar
-
-  digitalWrite(relayPin, HIGH);
-  // Imprimir o valor salvo do último acionamento
-  printLastPulseTime();
-  // Obter o tempo atual em millis
-  unsigned long tempoInicio = millis();
-  Serial.println("ligarReleComTempo");
-  // Aguardar até que tenha passado o tempo desejado
-  while (millis() - tempoInicio < ms) {
-    // Atraso não bloqueante
-    yield();
-  }
-  // Desligar o relé (assumindo LOW para desligar, ajuste conforme necessário)
+void acionarReleComTempo(unsigned int ms) {
+  digitalWrite(relayPin, HIGH);  // Ligar o relé
   saveLastPulseTime(timeClient.getEpochTime());
-  // desligarReleComTempo(intervalBetweenPulses);  //Desliga o rele
+  printLastPulseTime();  // Imprimir o valor salvo do último acionamento
+  Serial.print("Rele on: ");
+  Serial.println(timeClient.getEpochTime());
+
+  // Aguardar até que tenha passado o tempo desejado
+  delay(ms);
+
+  digitalWrite(relayPin, LOW);  // Desligar o relé
 }
 
 void desligarReleComTempo(unsigned int min) {
-  // Obter o tempo atual epoch em minutos
-  unsigned long agoraMinutos = timeClient.getEpochTime() / 60;
-  Serial.println("desligarReleComTempo");
-  digitalWrite(relayPin, LOW);
-  // Verificar se passou o intervalo desejado desde a última ativação
-  if (agoraMinutos - readLastPulseTime() >= min) {
-    // Chamar a função para desligar o relé (ajuste conforme necessário)
-    // Atualizar o tempo da última desativação em minutos
-    lastActivationTime = agoraMinutos;
+  unsigned long epoch = min * 60;
+  Serial.print("Rele off: ");
+  Serial.println(timeClient.getEpochTime());
+  Serial.print("epoch: ");
+  Serial.println(epoch);
+
+  // Aguardar até que tenha passado o intervalo desejado desde a última ativação
+  while (timeClient.getEpochTime() - readLastPulseTime() < epoch) {
+    delay(1000);  // Atraso de 1 segundo
   }
 }
 
@@ -140,21 +144,22 @@ void setup() {
   }
 
   timeClient.begin();
+
   // Inicia o sistema de arquivos SPIFFS
   if (!SPIFFS.begin()) {
     Serial.println("Falha ao montar o sistema de arquivos SPIFFS");
     return;
   }
-  listFiles();
-  hold(5000);  // Atraso de 5 segundo para evitar verificações muito rápidas
 }
 
 void loop() {
   timeClient.update();
+  
   // Acionar o relé
   acionarReleComTempo(pulseDuration);
+  
   // Desligar o relé após o intervalo desejado
   desligarReleComTempo(intervalBetweenPulses);
-
-  hold(1000);  // Atraso de 1 segundo para evitar verificações muito rápidas
+  //atualiza o valor  entre os pulsos
+  atualizarIntervaloEntrePulsos();
 }
